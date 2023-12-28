4c945d9a5edf73c9815a3a332b5aa531f55bbf7d
najwa balqis <nornajwatulbalqis20@gmail.com>
2023-12-28 13:43:58 +0800

    lAB4 FIREBASE

---
+++ app/src/main/java/com/example/lab4_balqis/Artist.java
@@ -0,0 +1,22 @@
+package com.example.lab4_balqis;
+
+public class Artist {
+
+    String artistId;
+    String artistName;
+
+    public Artist(){}
+
+    public Artist(String artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
